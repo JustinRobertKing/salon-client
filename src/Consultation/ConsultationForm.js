d4895a8fd053b5f025039b8578c39f062093d013
mayhemphone <mayhemphone@gmail.com>
2019-05-14 17:45:50 -0700

    image uploading works, not saved to db yet.  has a bug with last thumbnail

--- src/Consultation/ConsultationForm.js
+++ src/Consultation/ConsultationForm.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 class ConsultationForm extends Component {
-	
-	componentDidMount() {
-    
+
+componentDidMount(){
+	// let items = ''
+	// console.log('items', items)
+	// let imgArr=[]
 }
+
+state ={
+	profileImg: [],
+}
+
 
 checkUploadResult = (resultEvent) => {
 	if (resultEvent.event === 'success') {
-		console.log(resultEvent)
-		for (var i=0;i<resultEvent.length;i++){
-      let u = 'img_'+(i+1)
-      console.log(u)
-		}
-	
+
+
+		// imgArr.push(resultEvent)
+
+		console.log('resultEvent:',resultEvent.info.secure_url)
+		// this.setState({profileImg: resultEvent.info.secure_url})
+
+		this.setState({ profileImg: [...this.state.profileImg, resultEvent.info.secure_url] })
+
+		//loop through the uploaded images and do something with them
+		// imgArr.forEach((result,i)=>{
+		// 	console.log('url:',result.info.secure_url)
+			
+		// 	console.log('profileIMG', this.state.profileImg)
+		// })
+
 	}
+
 }
+
+
+
+
 
 showWidget = (widget) => {
 			window.cloudinary.openUploadWidget({
@@ -28,18 +52,21 @@ showWidget = (widget) => {
 
 	render() {
 
-		
+ let items = this.state.profileImg.map((item, i) => <div key={i}><img src={item} alt="Before Hair" key={ i } /></div>)
+ console.log('in the render---->',this.state.profileImg)
 
 		return (
 			 <div className="container">
 				 <Form>
 					<FormGroup>
-	          <Label for="exampleFile">File</Label>
-	          <Input type="file" name="file" id="exampleFile" />
 	          <FormText color="muted">
 	            Current hair photos
 	          </FormText>
-	          	
+	          	<div id="currentHair">
+		          	<Carousel>
+				         { items }
+								</Carousel>
+	          	</div>
          			<div id="photo-form-container">
          				<Button onClick={this.showWidget}>Upload Photo </Button>
          			</div>
