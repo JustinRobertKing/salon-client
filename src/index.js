baafd6aa5500a2f4419a973e69e21fe83c87092e
JustinRobertKing <kingj4@spu.edu>
2019-05-21 09:14:12 -0700

    turn in state

--- src/index.js
+++ src/index.js
@@ -6,10 +6,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(
-		<App />, document.getElementById('root'));
->>>>>>> 9e715260d200d481288e2c03a48a594f75dab1cf
 registerServiceWorker();
