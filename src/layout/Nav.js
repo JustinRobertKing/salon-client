e8cbf5e9af8ce82ca1014d8561a26a17ac89a01c
mayhemphone <mayhemphone@gmail.com>
2019-05-20 17:37:49 -0700

    styling update, including new logo, new nav position and icon, and stubbed stylist photo

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 class Nav extends Component {
   handleLogout = (e) => {
@@ -11,23 +13,20 @@ class Nav extends Component {
 
   render() {
     // let links = '';
-    if(this.props.user){
-      
-    
+  if(this.props.user){
+
     return(
         <div>
           <nav className="nav">
-            <span>
-            	<Link to="/">Home</Link>
-            	{this.props.user.stylist ?<Link to="/schedule">Schedule</Link>:'' }
-            </span>
-            <span>
-              <Link to="/profile">Me</Link>
+            {this.props.user.stylist ? <span><Link to="/">Home</Link><Link to="/schedule">Schedule</Link></span>:'' }
+            <span className='push'>
+              <Link to="/profile" ><FontAwesomeIcon icon="user-cog"/></Link>
             </span>
           </nav>
           <header className="App-header">
             <h6 className="welcome">Welcome, {this.props.user.firstname} </h6>
           </header>
+
         </div>
       );
     }
